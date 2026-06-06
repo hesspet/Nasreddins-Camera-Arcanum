d499cc857b18a344174f53db2abfa9ce0563a506
Nasr ad-Din al-Quffaz and Peter Heß (DE) <hesspet@gmx.de>
2026-06-06 16:23:28 +0200

    Neuer Spinner mit Arcanum Motiven

---
+++ Helpers/SpinnerImageCatalog.cs
@@ -0,0 +1,18 @@
+namespace Nasreddins_Camera_Arcanum.Helpers;
+
+/// <summary>
+/// Compile-time catalog of spinner images.
+/// Add new entries to <see cref="Images"/> to make them available
+/// for random selection at runtime.
+/// Images are served from wwwroot/images/spinner/.
+/// </summary>
+public static class SpinnerImageCatalog
+{
+    public static readonly IReadOnlyList<string> Images = new[]
+    {
+        "images/spinner/spinner1.png",
+        "images/spinner/spinner2.png",
+        "images/spinner/spinner3.png",
+        "images/spinner/spinner4.png",
+    };
+}
